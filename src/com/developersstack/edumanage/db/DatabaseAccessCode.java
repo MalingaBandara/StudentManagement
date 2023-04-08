34323f006e389619a23b3d56eaf9932df464a355
Malinga Bandara <malinga1021@gmail.com>
2023-04-08 18:55:34 +0530

    new user registration db access code

--- src/com/developersstack/edumanage/db/DatabaseAccessCode.java
+++ src/com/developersstack/edumanage/db/DatabaseAccessCode.java
@@ -1,9 +1,29 @@
 package com.developersstack.edumanage.db;
 
+import com.developersstack.edumanage.model.User;
+import com.developersstack.edumanage.util.security.PasswordManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DatabaseAccessCode {
 
         // User manage ===============>
+        public boolean saveUser(User user) throws SQLException,ClassNotFoundException   {
 
+            Connection connection = DbConnection.getInstance().getConnection();
+
+            PreparedStatement stm = connection.prepareStatement(" INSERT INTO user VALUES (?, ?, ?, ?)");
+
+            stm.setString( 1, user.getEmail() );
+            stm.setString( 2, user.getFirstName() );
+            stm.setString( 3, user.getFirstName() );
+            stm.setString( 4,  new PasswordManager().encrypt(user.getPassword() ));
+
+            return stm.executeUpdate() > 0;
+
+        }
 
         // User manage ===============>
 
