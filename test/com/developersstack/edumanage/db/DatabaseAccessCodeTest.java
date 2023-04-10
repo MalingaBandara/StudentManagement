f27a08d538c0637310c48b337a844c3a430f0f2e
Malinga Bandara <malinga1021@gmail.com>
2023-04-10 15:34:24 +0530

    test case

---
+++ test/com/developersstack/edumanage/db/DatabaseAccessCodeTest.java
@@ -0,0 +1,10 @@
+package com.developersstack.edumanage.db;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseAccessCodeTest {
+
+    @org.junit.jupiter.api.Test
+    void saveStudent() {
+    }
+}
