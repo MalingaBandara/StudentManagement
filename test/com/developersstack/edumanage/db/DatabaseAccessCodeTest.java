5f250dc61f461f872d1a370f5ffcd1735ae75be4
Malinga Bandara <malinga1021@gmail.com>
2023-04-10 15:46:04 +0530

    test case

--- test/com/developersstack/edumanage/db/DatabaseAccessCodeTest.java
+++ test/com/developersstack/edumanage/db/DatabaseAccessCodeTest.java
@@ -1,10 +1,28 @@
 package com.developersstack.edumanage.db;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import com.developersstack.edumanage.model.Student;
+
+import java.sql.SQLException;
+import java.util.Date;
 
 class DatabaseAccessCodeTest {
 
-    @org.junit.jupiter.api.Test
-    void saveStudent() {
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        new DatabaseAccessCodeTest().saveStudent();
+
+    }
+
+    void saveStudent() throws SQLException, ClassNotFoundException {
+
+        Student s = new Student( "S-1",  "Nimal", new Date(), "Colombo" );
+
+       boolean isSaved = new  DatabaseAccessCode().saveStudent( s );
+
+        System.out.println(isSaved);
+
     }
 }
