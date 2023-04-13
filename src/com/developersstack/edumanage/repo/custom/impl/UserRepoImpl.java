b4a8da18d66e5fba7d9dd90dd18e3ba068091a80
Malinga Bandara <malinga1021@gmail.com>
2023-04-13 21:29:41 +0530

    Update user impl class using crud util

--- src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
+++ src/com/developersstack/edumanage/repo/custom/impl/UserRepoImpl.java
@@ -1,11 +1,10 @@
 package com.developersstack.edumanage.repo.custom.impl;
 
-import com.developersstack.edumanage.db.DbConnection;
+
 import com.developersstack.edumanage.entity.User;
+import com.developersstack.edumanage.repo.CrudUtil;
 import com.developersstack.edumanage.repo.custom.UserRepo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -14,7 +13,7 @@ public class UserRepoImpl implements UserRepo {
     @Override
     public boolean saveUser(User user) throws SQLException,ClassNotFoundException   {
 
-        Connection connection = DbConnection.getInstance().getConnection();
+        /*Connection connection = DbConnection.getInstance().getConnection();
 
         PreparedStatement stm = connection.prepareStatement(" INSERT INTO user VALUES (?, ?, ?, ?)");
 
@@ -23,20 +22,37 @@ public class UserRepoImpl implements UserRepo {
         stm.setString( 3, user.getLastName() );
         stm.setString( 4, user.getPassword() );
 
-        return stm.executeUpdate() > 0;
+        return stm.executeUpdate() > 0;*/
+
+        return CrudUtil.execute( " INSERT INTO user VALUES (?, ?, ?, ?) ",
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPassword()
+        );
 
     }
 
     @Override
     public User loginUser( String email) throws SQLException,ClassNotFoundException   {
 
-        Connection connection = DbConnection.getInstance().getConnection();
+        /*Connection connection = DbConnection.getInstance().getConnection();
 
         PreparedStatement stm = connection.prepareStatement(" SELECT * FROM user WHERE email=? ");
 
         stm.setString( 1, email );
 
         ResultSet rst = stm.executeQuery();
+
+        if ( rst.next() ) {
+            return new User(
+                    rst.getString( 2 ), rst.getString( 3 ),
+                    rst.getString( 1 ), rst.getString( 4 ) );
+        }
+
+        return null;*/
+
+        ResultSet rst = CrudUtil.execute( " SELECT * FROM user WHERE email=? ", email  );
 
         if ( rst.next() ) {
             return new User(
